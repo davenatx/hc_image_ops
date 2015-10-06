cea0428ab16504e9b9534c6de5c073cd616d583a
David Price <dprice@austindata.com>
2015-10-06 17:27:52 -0500

    Changed sucessfull logging statement to INFO.  Also configured the main method

--- src/main/scala/ImageOveraly.scala
+++ src/main/scala/ImageOveraly.scala
@@ -8,12 +8,10 @@ import com.typesafe.scalalogging.LazyLogging
 object ImageOverlay extends LazyLogging {
 
   /**
-   * Return the "cropped images" from the database and process each record overlaying
-   * the image onto a new one
+   * Process a list of ImageRecords and overlay the images
    */
-  def overlayCroppedImages {
-    /* Retrieve cropped image records from database */
-    DBImageRecord.croppedImages map (record => {
+  def overlayCroppedImages(records: List[ImageRecord]) {
+    records map (record => {
 
       val f = new File(record.filePath, record.fileName)
 
@@ -22,14 +20,14 @@ object ImageOverlay extends LazyLogging {
         val newImage = OverlayImage.overlay(img, record.imageWidth.toInt, overlayLength)
         TIFFImage.toFile(f, List(newImage))
       }) match {
-        case Some(true) => logger.trace("Image record with filename: {} sucessfully overlayed", record.fileName)
+        case Some(true) => logger.info("Image record with filename: {} sucessfully overlayed", record.fileName)
         case Some(false) => logger.warn("Image record with filename: {} failed to overlay", record.fileName)
         case None => logger.warn("Something went wrong processing file: {}", record.fileName)
       }
 
       /* Update the IS_OVERALYED database field to true */
       updateIsOverlayed(record, true) match {
-        case 1 => logger.trace("Image record with filename: {} sucessfully updated", record.fileName)
+        case 1 => logger.info("Image record with filename: {} sucessfully updated", record.fileName)
         case _ => logger.warn("Image record with filename: {} failed to update IS_OVERLAYED field", record.fileName)
       }
     })
@@ -40,6 +38,7 @@ object ImageOverlay extends LazyLogging {
  * Overlay the cropped images
  */
 object OverlyaImages extends App {
-
+  /* Retrieve cropped image records from database and overlay */
+  ImageOverlay(DBImageRecord.croppedImages)
 }
 
